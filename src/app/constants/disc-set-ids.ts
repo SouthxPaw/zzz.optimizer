4a7452e0b87f943e7f2dc86899cf10d0c281bfd3
Southpaw <jordanaletraris@gmail.com>
2026-01-13 08:43:23 -0500

    Improving UI and performance boosting

---
+++ src/app/constants/disc-set-ids.ts
@@ -0,0 +1,30 @@
+/**
+ * Disc set equipment IDs from the game data
+ * These correspond to the JSON files in assets/data/equipment/
+ */
+export const DISC_SET_EQUIPMENT_IDS = [
+  '31000', // Woodpecker Electro
+  '31100', // Puffer Electro
+  '31200', // Polar Metal
+  '31300', // Inferno Metal
+  '31400', // Thunder Metal
+  '31500', // Shockstar Disco
+  '31600', // Swing Jazz
+  '31800', // Soul Rock
+  '31900', // Proto Punk
+  '32200', // Freedom Blues
+  '32300', // Hormone Punk
+  '32400', // Chaotic Metal
+  '32500', // Fanged Metal
+  '32600', // Astral Voice
+  '32700', // Branch & Blade Song
+  '32800', // Starlight Engine
+  '32900', // Thunder Metal (Alternative)
+  '33000', // Woodpecker Electro (Alternative)
+  '33100', // Polar Metal (Alternative)
+  '33200', // Inferno Metal (Alternative)
+  '33300', // Puffer Electro (Alternative)
+  '33400', // Drive Disc - Unknown
+  '33500', // Drive Disc - Unknown
+  '33600', // Drive Disc - Unknown
+] as const;
