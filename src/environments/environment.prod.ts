292fca5c3eabe6ef594fbf266cbf3e7fe71c7f15
Southpaw <jordanaletraris@gmail.com>
2026-02-01 16:05:32 -0500

    Changing how we call the enka api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ import packageJson from '../../package.json';
 
 export const environment = {
   production: true,
-  appVersion: packageJson.version
+  appVersion: packageJson.version,
+  // Cloudflare Worker URL for Enka API proxy
+  enkaProxyUrl: 'https://zzzoptimizer.southxpaw.workers.dev'
 };
