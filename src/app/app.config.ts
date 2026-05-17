4c081905c5416e68c122c2c9fc58169a99a4dda3
Southpaw <jordanaletraris@gmail.com>
2026-05-17 12:50:56 -0400

    Changelog and SW changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,7 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
-            registrationStrategy: 'registerImmediately'
+            registrationStrategy: 'registerWhenStable'
           })
   ]
 };
